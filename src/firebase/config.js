1623109d8776f1b21e94495e88067b774c2caff0
Fabiola de la Cruz <itzeel.1504@gmail.com>
2024-10-03 19:31:31 -0600

    configuracion firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPb49YSYjnIcgwpdh_sbYicbF9G9kVs7M",
+  authDomain: "beauty-salon-e0bac.firebaseapp.com",
+  projectId: "beauty-salon-e0bac",
+  storageBucket: "beauty-salon-e0bac.appspot.com",
+  messagingSenderId: "488993769919",
+  appId: "1:488993769919:web:1ca497a345c47fe5fc390a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
